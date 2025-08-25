77051bbe54162667b9bbc858670c03527c5d4743
dungminh0306-thanhhoa <dungminh0306@gmail.com>
2025-08-25 13:25:51 +0700

    Update DEMO.py

--- DEMO.py
+++ DEMO.py
@@ -14,7 +14,7 @@ def gdrive_to_direct(link):
 # Danh sách sản phẩm test
 products = [
     {"id": 1, "name": "Áo thun", "price": 120000,
-     "image": "https://drive.google.com/file/d/1s6sJALOs2IxX5f9nqa4Tf8zut_U9KE3O/view?usp=sharing"},
+     "image": "https://drive.google.com/file/d/1s6sJALOs2IxX5f9nqa4Tf8zut_U9KE3O/view?usp=drive_link"},
     {"id": 2, "name": "Quần jean", "price": 250000,
      "image": "https://via.placeholder.com/150"},
     {"id": 3, "name": "Áo khoác", "price": 350000,
@@ -26,3 +26,4 @@ st.title("🖼️ Test hiển thị ảnh Google Drive")
 for p in products:
     st.image(gdrive_to_direct(p["image"]), caption=p["name"], width=200)
     st.write(f"Giá: {p['price']:,} VND")
+
